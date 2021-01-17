3ae190f1412b28443eff966d81cedf7a0e0c2fca
Dawid Stefanik <dstefanik@student.agh.edu.pl>
2021-01-17 14:47:11 +0100

    Gotowe

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -3,31 +3,44 @@ package pl.edu.agh.mwo.invoice.product;
 import java.math.BigDecimal;
 
 public abstract class Product {
-    private final String name;
+	private final String name;
 
-    private final BigDecimal price;
+	private final BigDecimal price;
 
-    private final BigDecimal taxPercent;
+	private final BigDecimal taxPercent;
 
-    protected Product(String name, BigDecimal price, BigDecimal tax) {
-        this.name = name;
-        this.price = price;
-        this.taxPercent = tax;
-    }
+	protected Product(String name, BigDecimal price, BigDecimal tax) {
 
-    public String getName() {
-        return null;
-    }
+		if (name == null || name.equals("")) {
+			throw new IllegalArgumentException(
+				"You cannot create products with null or empty name");
+		}
 
-    public BigDecimal getPrice() {
-        return null;
-    }
+		this.name = name;
+		
+		
+		if (price == null || price.compareTo(new BigDecimal("0")) == -1) {   //price.signum() = -1
+			throw new IllegalArgumentException(
+				"You cannot create products with null or negative price");
+		}
+		
+		this.price = price;
+		this.taxPercent = tax;
+	}
 
-    public BigDecimal getTaxPercent() {
-        return null;
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public BigDecimal getPriceWithTax() {
-        return null;
-    }
+	public BigDecimal getPrice() {
+		return this.price;
+	}
+
+	public BigDecimal getTaxPercent() {
+		return this.taxPercent;
+	}
+
+	public BigDecimal getPriceWithTax() {
+		return this.price.add(this.price.multiply(this.taxPercent));
+	}
 }
